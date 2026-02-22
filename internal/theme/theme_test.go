164534debe23b0f1596d25893a15ea28b9b806a4
CLHatch <CLHatch@gmail.com>
2026-02-22 02:39:16 -0600

    Fix build failure by removing obsolete TestParseColor from theme test

--- internal/theme/theme_test.go
+++ internal/theme/theme_test.go
@@ -16,11 +16,11 @@ func TestGetColorStr(t *testing.T) {
 		expected string
 	}{
 		// Standard Colors (converted to hex in lipgloss v2)
-		{lipgloss.Color("0"), "#000000"},   // black
-		{lipgloss.Color("1"), "#800000"},   // red (ANSI)
-		{lipgloss.Color("2"), "#008000"},   // green
-		{lipgloss.Color("4"), "#000080"},   // blue
-		{lipgloss.Color("7"), "#c0c0c0"},   // white (silver)
+		{lipgloss.Color("0"), "#000000"}, // black
+		{lipgloss.Color("1"), "#800000"}, // red (ANSI)
+		{lipgloss.Color("2"), "#008000"}, // green
+		{lipgloss.Color("4"), "#000080"}, // blue
+		{lipgloss.Color("7"), "#c0c0c0"}, // white (silver)
 
 		// Custom RGB
 		{lipgloss.Color("#010203"), "#010203"},
@@ -40,34 +40,4 @@ func TestGetColorStr(t *testing.T) {
 	}
 
 	testutils.PrintTestTable(t, cases)
-}
-
-func TestParseColor(t *testing.T) {
-	tests := []struct {
-		input    string
-		expected string
-	}{
-		// Standard Colors (Now expecting Hex)
-		{"black", "#000000"},
-		{"red", "#ff0000"},
-
-		// Hex Codes
-		{"#123456", "#123456"},
-
-		// Tcell Extended Colors
-		{"orange", "#ffa500"}, // CSS Orange
-		{"rebeccapurple", "#663399"},
-	}
-
-	for _, tt := range tests {
-		c := parseColor(tt.input)
-		actual := ""
-		if c != nil {
-			actual = console.GetColorStr(c)
-		}
-
-		if actual != tt.expected {
-			t.Errorf("parseColor(%q) = %q, want %q", tt.input, actual, tt.expected)
-		}
-	}
 }
